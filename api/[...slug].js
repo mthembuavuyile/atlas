7c7c4a7a04e122ee8f9b8764eab303a5f90cfe2a
mthembuavuyile <mthembuavuyile@gmail.com>
2026-09-08 11:22:31 +0200

    fix: use native Vercel catch-all slug routing for api directory

--- api/[...slug].js
+++ api/[...slug].js
@@ -1,5 +1,22 @@
 const app = require('../src/server');
 
 module.exports = (req, res) => {
+  // Reconstruct req.url from Vercel catch-all slug
+  if (req.query && req.query.slug) {
+    const slug = Array.isArray(req.query.slug) ? req.query.slug.join('/') : req.query.slug;
+    const urlParams = new URLSearchParams();
+    for (const [key, value] of Object.entries(req.query)) {
+      if (key !== 'slug') {
+        if (Array.isArray(value)) {
+          value.forEach(v => urlParams.append(key, v));
+        } else {
+          urlParams.append(key, value);
+        }
+      }
+    }
+    const qs = urlParams.toString();
+    req.url = '/api/' + slug + (qs ? '?' + qs : '');
+  }
+
   return app(req, res);
 };
